77232b539d3f76de0ccd290b34097f0ae72eb889
Olvet7 <volya.maslova@gmail.com>
2024-02-20 19:17:53 +0200

    css (fit in screen)

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,12 +1,16 @@
+import css from "../Articles/Articles.module.css"
+
 export function Articles ({article}) {
     return (
         <div>
-        <h2>{article.title}</h2>
+        <p>{article.title}</p>
         <p>{article.desc}</p>
         <img src={article.text} alt={article.title} width='400px'/>
-        <ul>
+        <ul className={css.list}>
           <li>{article.p1}</li>
+          <hr />
           <li>{article.p2}</li>
+          <hr />
           <li>{article.p3}</li>
         </ul>
       </div>
