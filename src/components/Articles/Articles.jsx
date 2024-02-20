562b63ab4885504ef999ffac1d87404b3787e280
Olvet7 <volya.maslova@gmail.com>
2024-02-20 19:09:43 +0200

    quotes added

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -4,6 +4,11 @@ export function Articles ({article}) {
         <h2>{article.title}</h2>
         <p>{article.desc}</p>
         <img src={article.text} alt={article.title} width='400px'/>
+        <ul>
+          <li>{article.p1}</li>
+          <li>{article.p2}</li>
+          <li>{article.p3}</li>
+        </ul>
       </div>
     )
 }
